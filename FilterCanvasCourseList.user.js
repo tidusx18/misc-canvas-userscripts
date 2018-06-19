536bbe3daddee77db53ce10ef42077d6407daf83
tidusx18 <victoriano518@gmail.com>
2018-06-19 17:51:35 -0400

    Add: Filter Canvas Course List userscript

---
+++ FilterCanvasCourseList.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Canvas Util: Filter Course List
+// @namespace    https://github.com/tidusx18
+// @version      0.0.1
+// @description  Filters courses based on user input in course list page
+// @author       Daniel Victoriano <victoriano518@gmail.com>
+// @match        https://fiu.instructure.com/courses
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+	let rows = document.querySelectorAll('#content table tr');
+	let header = document.querySelector('.header-bar');
+	let input = document.createElement('input');
+	let button = document.createElement('button');
+
+	button.innerText = 'Submit';
+
+	header.appendChild(input);
+	header.appendChild(button);
+
+	button.addEventListener('click', filter);
+
+	function filter() {
+
+		rows.forEach( (row) => {
+
+			let regex = new RegExp(input.value);
+
+			if(!regex.test(row.innerText)) { row.style = 'display: none;' }
+
+		});
+
+	}
+
+})();
