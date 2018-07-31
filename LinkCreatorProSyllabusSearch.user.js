0c948e4b25711aaac50f58adb0187fb9dfd1d794
tidusx18 <victoriano518@gmail.com>
2018-07-31 15:01:59 -0400

    Update: Remove for loop and cleanup code.

--- LinkCreatorProSyllabusSearch.user.js
+++ LinkCreatorProSyllabusSearch.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Canvas UI: Link to CreatorPro Syllabus Search
+// @name         Canvas UI: CreatorPro Edit Quick Link
 // @namespace    http://github.com/tidusx18
 // @version      0.0.1
 // @description
@@ -12,64 +12,46 @@
 
 (function() {
 
-	const menuItems = [
-		{
-			title: 'Edit Syllabus',
-			url: buildCreatorProUrl(),
-			addToTop: true
-		}
-	];
+	const menuItem = {
+			title: 'Edit Syllabus'
+		};
 
-	function buildMenu() {
-		const menuEl = document.querySelector( '#section-tabs' );
-
-		menuEl.appendChild( document.createElement( 'hr' ) );
-
-		for ( let i = 0; i < menuItems.length; i++ ) {
-			let itemEl = makeMenuItem( menuItems[ i ].title, menuItems[ i ].url, menuItems[ i ].method );
-			menuEl.appendChild( itemEl );
-		}
-	}
-
-	function makeMenuItem( title, url, method ) {
-		const baseUrl = window.location.href.split( '/courses/' )[ 0 ];
-		const courseID = window.location.href.split( '/' )[ 4 ];
+	function makeMenuItem( title, url ) {
 		const itemEl = document.createElement( 'li' );
 		const linkEl = document.createElement( 'a' );
 
 		itemEl.classList.add( 'section' );
 		linkEl.setAttribute( 'href', `${url}` );
 		linkEl.setAttribute('target', '_blank');
-
-		if ( method ) {
-			// Unsure if other methods work. Currently only tested post
-			linkEl.setAttribute( 'data-method', method );
-		}
-
 		linkEl.innerHTML = title;
+
 		itemEl.appendChild( linkEl );
 
 		return itemEl;
 	}
 
-	function buildCreatorProUrl() {
+	function buildMenu() {
+		const menuEl = document.querySelector( '#section-tabs' );
+		let itemEl = makeMenuItem( menuItem.title, menuItem.url );
 
+		menuEl.appendChild( document.createElement( 'hr' ) );
+		menuEl.appendChild( itemEl );
+	}
+
+	function init() {
 	let url = document.location.href.match(/https:\/\/fiu.instructure.com\/courses\/\d+/i)[0];
-	url = document.location.href.replace('.com', '.com/api/v1');
+	url = url.replace('.com', '.com/api/v1');
 
-        fetch(`${url}?per_page=80`, { credentials: 'include' })
+        fetch(url, { credentials: 'include' })
             .then(data => data.text())
             .then(data => {
+				let course = JSON.parse(data.replace('while(1);', ''));
+				menuItem.url = `https://cp.fiu.edu/?filter=all&q=${course.sis_course_id}&lms=&term=all&status=all`;
 
-			let course = JSON.parse(data.replace('while(1);', ''));
-
-			// menuItems[0].url = `${course.sis_course_id}`;
-			menuItems[0].url = `https://cp.fiu.edu/?filter=all&q=${course.sis_course_id}&lms=&term=all&status=all`;
-
-			buildMenu();
-
+				buildMenu();
             });
-
 	}
+
+	init();
 
 })();
